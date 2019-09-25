450d95ee8956554d21f9ea054c289c7da9eaf4a1
Josue <josgar_14@hotmail.com>
2019-09-24 22:01:38 -0400

    filling form controls with data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
-
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  title = 'forms-update';
+	title = 'forms-update';
+	myFormModel: FormGroup;
+
+	constructor() {
+		this.myFormModel = new FormGroup({
+			id: new FormControl(''),
+			description: new FormControl(''),
+			seller: new FormControl('')
+		});
+	}
+
+	updateEntireForm() {
+		this.myFormModel.setValue({
+			id: 123,
+			description: 'A great product',
+			seller: 'XYZ Corp'
+		});
+	}
+	updatePartOfTheForm() {
+		this.myFormModel.patchValue({
+			description: 'The best product'
+		});
+	}
 }
